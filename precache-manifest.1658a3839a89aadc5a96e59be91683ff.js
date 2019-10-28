e54c87dd19263d6e6a3431b50befb66a3e33bfcd
Jadir Ibna Hasan <jhasan172089@bscse.uiu.ac.bd>
2019-10-28 02:03:22 -0700

    Updates

---
+++ precache-manifest.1658a3839a89aadc5a96e59be91683ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb27412de505ae1b0723e47443aa02",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "c70f0b9f21255efe4d0c",
+    "url": "/app/static/css/2.7c4efcaa.chunk.css"
+  },
+  {
+    "revision": "9c1107842f741ac59944",
+    "url": "/app/static/css/main.2f954dfb.chunk.css"
+  },
+  {
+    "revision": "c70f0b9f21255efe4d0c",
+    "url": "/app/static/js/2.cc540091.chunk.js"
+  },
+  {
+    "revision": "9c1107842f741ac59944",
+    "url": "/app/static/js/main.d2f67adc.chunk.js"
+  },
+  {
+    "revision": "599749dd40221f3c071d",
+    "url": "/app/static/js/runtime-main.1e7aac68.js"
+  }
+]);
